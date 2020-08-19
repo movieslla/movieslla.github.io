142facafc3dbd133ef11f66acfd474be9af1d535
movieslla <movieslla000@gmail.com>
2020-08-19 20:58:27 +0530

    new

---
+++ js/content.js
@@ -0,0 +1,18 @@
+var content = [
+    {
+        id:1,
+        img: "https://akamaividz2.zee5.com/image/upload/w_261,h_147,c_scale,f_auto,q_auto/resources/0-1-manual_1ebkhvibbdu8/list/sembaruthi_ep772_seamless_.jpg",
+        date: "New Event . 12 July 2018",
+        code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-jevt2cbat204.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>',
+
+    },
+    {
+        id:2,
+        img: "https://akamaividz2.zee5.com/image/upload/w_261,h_147,c_scale,f_auto,q_auto/resources/0-1-manual_1kb7874uivkg/list/sembaruthi_ep771_seamless_.jpg",
+        date: "New Event . 12 July 2018",
+        code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-jevt2cbat204.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>',
+
+    },
+
+]
+
---
+++ js/main.js
@@ -0,0 +1,30 @@
+var mainCont = {
+    code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-w21rkf3a1hf7.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>'
+};
+
+document.getElementById('mainvid').innerHTML = mainCont.code;
+
+
+content.forEach((i) => {
+    document.getElementById('cont').innerHTML += `
+    <div class="card" onclick="vidselect(3)">
+    <img src=${i.img} alt="Image" class="ima">
+    <p class="tit">${i.date}</p>
+    </div>
+
+    `
+})
+
+function vidselect(myid) {
+    content.forEach((i) => {
+        if (i.id == myid) {
+            document.getElementById('mainvid').innerHTML = i.code;
+        }
+    })
+    $(window).scrollTop(100); 
+};
+
+
+
+
+
