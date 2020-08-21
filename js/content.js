29cc77b98139abb5d7d171c97a036b32aa2114a9
movieslla <movieslla000@gmail.com>
2020-08-21 09:18:14 +0530

    next one

--- js/content.js
+++ js/content.js
@@ -1,20 +1,39 @@
 var content = [
     {
         id: 1,
-        img: "https://akamaividz2.zee5.com/image/upload/w_261,h_147,c_scale,f_auto,q_auto/resources/0-1-manual_1ebkhvibbdu8/list/sembaruthi_ep772_seamless_.jpg",
-        date: "New Event . 12 July 2018",
-        code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-jevt2cbat204.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>',
-        d:"google.com",
+        img: "https://akamaividz2.zee5.com/image/upload/w_368,h_207,c_scale,f_auto,q_auto/resources/0-1-manual_1kb7874uivkg/list/sembaruthi_ep771_seamless_.jpg",
+        date: "17 Aug 2020, Monday - E771",
+        code: '<iframe id="tm-welcome-video" class="tm-welcome-video" src="https://streamtape.com/e/Q1XOR433x9T06x8/www.sembaruthi.online_-_Episode_771.mp4" width="1" height="1" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
+        d: "https://streamtape.com/v/Q1XOR433x9T06x8",
     },
     {
         id: 2,
-        img: "https://akamaividz2.zee5.com/image/upload/w_261,h_147,c_scale,f_auto,q_auto/resources/0-1-manual_1kb7874uivkg/list/sembaruthi_ep771_seamless_.jpg",
-
-        date: "New Event . 12 July 2018",
-        code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-w21rkf3a1hf7.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>',
-        d:"youtube.com",
-
+        img: "https://akamaividz2.zee5.com/image/upload/w_368,h_207,c_scale,f_auto,q_auto/resources/0-1-manual_1ebkhvibbdu8/list/sembaruthi_ep772_seamless_.jpg",
+        date: "18 Aug 2020, Tuesday - E772",
+        code: '<iframe id="tm-welcome-video" class="tm-welcome-video" src="https://streamtape.com/e/JWBr8rR8lGsGzd/www.sembaruthi.online_-_Episode_772.mp4" width="1" height="1" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
+        d: "https://streamtape.com/v/JWBr8rR8lGsGzd",
+    },
+    {
+        id: 3,
+        img: "https://akamaividz2.zee5.com/image/upload/w_368,h_207,c_scale,f_auto,q_auto/resources/0-1-manual_29djidmth9qg/list/sembaruthi_ep773_seamless_.jpg",
+        date: "19 Aug 2020, Wednesday - E773",
+        code: '<iframe id="tm-welcome-video" class="tm-welcome-video" src="https://streamtape.com/e/4q6pey6YLyfKa14/www.sembaruthi.online_-_Episode_773.mp4" width="1" height="1" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
+        d: "https://streamtape.com/v/4q6pey6YLyfKa14",
+    },
+    {
+        id: 4,
+        img: "https://akamaividz2.zee5.com/image/upload/w_368,h_207,c_scale,f_auto,q_auto/resources/0-1-manual_6614v4molls0/list/01manual_6614v4_1266921036.jpg",
+        date: "20 Aug 2020, Thursday - E774",
+        code: '<iframe id="tm-welcome-video" class="tm-welcome-video" src="https://streamtape.com/e/voa99mzmbdu4GQP/www.sembaruthi.online_-_Episode_774.mp4" width="1" height="1" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
+        d: "https://streamtape.com/v/voa99mzmbdu4GQP",
+    },
+    {
+        id: 5,
+        img: "https://akamaividz2.zee5.com/image/upload/w_368,h_207,c_scale,f_auto,q_auto/resources/0-1-manual_1cfrapm9p1tg/list/01manual_1cfrap_1540756358.jpg",
+        date: "21 Aug 2020, Friday - E775",
+        code: '<iframe id="tm-welcome-video" class="tm-welcome-video" src="https://streamtape.com/e/AAyrXOmgz9SXrja/Episode_775.mp4" width="1" height="1" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
+        d: "https://streamtape.com/v/AAyrXOmgz9SXrja",
     },
 
 ]
-
+// id="tm-welcome-video" class="tm-welcome-video"
--- js/main.js
+++ js/main.js
@@ -1,36 +1,22 @@
-var mainCont = {
-    code: '<IFRAME id="tm-welcome-video" class="tm-welcome-video" SRC="https://oogly.io/embed-w21rkf3a1hf7.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO allowfullscreen></IFRAME>'
-};
-
-document.getElementById('mainvid').innerHTML = mainCont.code;
-document.getElementById('dlinkdiv').innerHTML = `
-<a href="#" id="dlink"> Download Link</a>
-`;
-
-
-
-content.forEach((i) => {
+document.getElementById('mainvid').innerHTML = content[content.length - 1].code;
+document.getElementById('dlinkdiv').innerHTML = `<a href=${content[content.length - 1].d} id="dlink"> Download Link</a>`;
+content.reverse().forEach((i) => {
     document.getElementById('cont').innerHTML += `
     <div class="card" onclick="vidselect(${i.id})">
     <img src=${i.img} alt="Image" class="ima">
     <p class="tit">${i.date}</p>
-    </div>
-
-    `
+    </div>`
 })
-
 function vidselect(myid) {
-	console.log(myid);
+    console.log(myid);
     content.forEach((i) => {
         if (i.id == myid) {
             document.getElementById('mainvid').innerHTML = i.code;
             document.getElementById('dlinkdiv').innerHTML = `
             <a href=${i.d} id="dlink"> Download Link</a>
-            `
-
-        }
+            `}
     })
-    $(window).scrollTop(100); 
+    $(window).scrollTop(100);
 };
 
 
